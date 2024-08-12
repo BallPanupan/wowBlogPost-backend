540a31b607521db8545c7bc68d2f3e5bdad49cec
Panupan <Ball085573@hotmail.com>
2024-08-12 21:24:40 +0700

    style()

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,3 @@
-// src/profile/profile.service.ts
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ProfileDto } from './dto/profile.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -12,7 +11,6 @@ export class ProfileService {
   ) {}
 
   async getProfileById(id: any): Promise<ProfileDto> {
-    console.log('userId xxx', id);
     const profile = await this.userModel.findById(id, {__v:0}).exec();
     if (!profile) throw new NotFoundException(`Profile with ID ${id} not found`);
     return profile.toObject() as ProfileDto;
